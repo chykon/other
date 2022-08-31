1769dec141b13dc021b2e51e587038ed87872d2d
Chykon <82272369+chykon@users.noreply.github.com>
2022-08-31 14:10:37 +0300

    Add "--accept-tos" flag

--- instructions/fedora/files/usr/local/bin/warpfix.sh
+++ instructions/fedora/files/usr/local/bin/warpfix.sh
@@ -5,11 +5,11 @@ while true; do
   if ! timeout 1 ping -4 -c 1 one.one.one.one; then
     if [ "$switch" -eq 0 ]; then
       echo 'switch:0'
-      warp-cli set-gateway 0
+      warp-cli --accept-tos set-gateway 0
       switch=1
     else
       echo 'switch:1'
-      warp-cli clear-gateway
+      warp-cli --accept-tos clear-gateway
       switch=0
     fi
   fi
